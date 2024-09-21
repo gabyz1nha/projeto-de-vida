01d727f2c34e4c8d1734b6ec9d23b90690428562
gabyz1nha <160290556+gabyz1nha@users.noreply.github.com>
2024-09-20 22:09:37 -0300

    Add files via upload

---
+++ password.js
@@ -0,0 +1,26 @@
+constnumeroSenha=document.querySelector('.paramentro-senha_texto');
+lettamanhoSenha = 12;
+numeroSenha.textContent=tamanhoSenha;
+constletrasMaiusculas='ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+constletrasMinusculas='abcdefghijklmnopqrstuvwxyz';
+construmeros='0123456789';
+constsimbolos='!@%*?';
+constbotoes=document.querySelectorAll('.parametro-senha_botao');
+constcampoSenha=document.querySelector(#campo-senha);
+constcheckbox=document.querySelectorAll(.checkbox);
+constforcaSenha=document.querySelector(.forca);
+
+
+botoes[0].onclick=diminuiTamanho;
+botoes[1].onclick=aumentaTamanho;
+
+functiondiminuiTamanho(){
+    if (tamanhoSenha>1){
+        //tamanhoSenha=tamanhoSenha-1;
+        tamanhoSenha--;
+
+    }
+
+    functionaumentaTamanho() {
+     if   
+    }
